bfc22bf85a6ad0c05cc1d724fbb7dac34f7fbe8e
KWangechi <rachel.karanja@strathmore.edu>
2025-01-02 20:35:57 +0300

    feat: added folders to the project structure

---
+++ src/pages/auth/RegisterPage.tsx
@@ -0,0 +1,23 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+import { z } from "zod";
+import { userSchema } from "../../utils/zodSchema";
+import { User } from "../../utils/types";
+import { useForm } from "react-hook-form";
+
+const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<User>({
+    resolver: zodResolver(userSchema),
+  });
+
+  
+};
+
+export default Register;
