0c6cecb14bc3a4223a70e1e590b3f81f0166ceb4
KWangechi <rachel.karanja@strathmore.edu>
2025-01-03 12:13:08 +0300

    refactor: removed unused imports for now

--- src/pages/auth/RegisterPage.tsx
+++ src/pages/auth/RegisterPage.tsx
@@ -1,21 +1,21 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect, useState } from "react";
-import { z } from "zod";
-import { userSchema } from "../../utils/zodSchema";
-import { User } from "../../utils/types";
-import { useForm } from "react-hook-form";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useEffect, useState } from "react";
+// import { z } from "zod";
+// import { userSchema } from "../../utils/zodSchema";
+// import { User } from "../../utils/types";
+// import { useForm } from "react-hook-form";
 
 const Register = () => {
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    watch,
-    reset,
-    formState: { errors },
-  } = useForm<User>({
-    resolver: zodResolver(userSchema),
-  });
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   setValue,
+  //   watch,
+  //   reset,
+  //   formState: { errors },
+  // } = useForm<User>({
+  //   resolver: zodResolver(userSchema),
+  // });
 
   
 };
